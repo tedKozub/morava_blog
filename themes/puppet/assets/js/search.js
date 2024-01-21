258f447d5372e1dffae2d9a2ef23b7f11f730e7d
Tadeáš Kozub <xkozub1@fi.muni.cz>
2024-01-21 13:42:00 +0100

    fix search input not returning scrollbar after it closes

--- themes/puppet/assets/js/search.js
+++ themes/puppet/assets/js/search.js
@@ -28,27 +28,54 @@ SimpleJekyllSearch({
 });
 
 $(document).ready(function () {
-  var $searchPage = $('.search-page');
-  var $searchOpen = $('.search-icon');
-  var $searchClose = $('.search-icon-close');
-  var $searchInput = $('#search-input');
-  var $body = $('body');
+    var $searchPage = $('.search-page');
+    var $searchOpen = $('.search-icon');
+    var $searchClose = $('.search-icon-close');
+    var $searchInput = $('#search-input');
+    var $body = $('body');
 
-  $searchOpen.on('click', function (e) {
-      e.preventDefault();
-      $searchPage.toggleClass('search-active');
-      var prevClasses = $body.attr('class') || '';
-      setTimeout(function () {
-          $body.addClass('no-scroll');
-      }, 400)
+    // Function to open the search modal
+    function openSearchModal() {
+        $searchPage.addClass('search-active');
+        var prevClasses = $body.attr('class') || '';
+        setTimeout(function () {
+            $body.addClass('no-scroll');
+        }, 400);
 
-      if ($searchPage.hasClass('search-active')) {
-          $searchClose.on('click', function (e) {
-              e.preventDefault();
-              $searchPage.removeClass('search-active');
-              $body.attr('class', prevClasses);  // from closure 
-          });
-          $searchInput.focus();
-      }
-  });
+        $searchClose.on('click', function (e) {
+            e.preventDefault();
+            $searchPage.removeClass('search-active');
+            $body.attr('class', prevClasses);
+        });
+
+        $searchInput.focus();
+    }
+
+    // Click event for opening the search modal
+    $searchOpen.on('click', function (e) {
+        e.preventDefault();
+        openSearchModal();
+    });
+
+    // Keyboard shortcut to open and close the search modal (Ctrl + K)
+    // and close (Escape)
+    $(document).on('keydown', function (e) {
+        if (e.ctrlKey && e.key === 'k' && $searchPage.hasClass('search-active')) {
+            $searchPage.removeClass('search-active');
+            $body.removeClass('no-scroll');
+        }
+        else if (e.ctrlKey && e.key === 'k') {
+            e.preventDefault();
+            openSearchModal();
+        }
+        else if (e.key === 'Escape') {
+            // the first escape press cancels the cursor in text input field
+            // so there are two esc presses needed to actually close the modal 
+            // but I guess that is expected behavior, so I am gonna let it be 
+            // for now
+            e.preventDefault();
+            $searchPage.removeClass('search-active');
+            $body.removeClass('no-scroll');
+        }
+    });
 });
